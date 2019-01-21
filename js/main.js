5dd4c96ddf3d350d0f579e2540b4a133d42babd5
徐龙山 <longshan.wp7@hotmail.com>
2019-01-21 14:10:52 +0800

    上传

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Avoid `console` errors in browsers that lack a console.
+
+
+// Window resized
+
+
+
+/** Clock **/
+
+
+/** Customization **/
+
+
+
+/** Static video background **/
+
+
+
+
+/** Init fullpage.js */
+$(document).ready(function() {
+    $('#mainpage').fullpage({
+        licenseKey: 'D2DB0F76-42B04C3A-938789B7-AE7D1A8F',
+        anchors: ['top', 'bing', 'vs', 'vedio', 'product','weizhi','weixin'],
+        //sectionsColor: ['#F5F5F5', '#FEF3E0'],
+        css3: true,
+        //navigation: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        lazyLoad: true,
+        //scrollOverflow: true
+        //controlArrows: false,
+      
+    });
+});
+
+
+/** Clock **/
+
+
+
+
+
+
+/* Page Loader : hide loader when all are loaded */
+
+/* END OF Page Loader : hide loader when all are loaded */
